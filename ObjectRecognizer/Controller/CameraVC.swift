d02bcd6d9b7aa1747f67090b07e8dbe473814d50
stahamnguyen <nguyensnj@gmail.com>
2017-10-09 19:34:00 +0300

    add cameraSession and related components

--- ObjectRecognizer/Controller/CameraVC.swift
+++ ObjectRecognizer/Controller/CameraVC.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
+    
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     @IBOutlet weak var infoBackgroundView: CustomView!
     @IBOutlet weak var capturedImageView: CustomImageView!
@@ -20,6 +25,42 @@ class CameraVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput!)
+            }
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer.videoGravity = .resizeAspect
+            previewLayer.connection?.videoOrientation = .portrait
+            
+            cameraView.layer.addSublayer(previewLayer!)
+            captureSession.startRunning()
+        } catch {
+            print(error)
+        }
     }
 
 
